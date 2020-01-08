09a3b6690b1bde5e6af6b0ac0ec98e95d2c5e234
ericauv <eriauv17@gmail.com>
2020-01-08 10:30:33 -0500

    remove logo, add name test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import {
+  render,
+  getByPlaceholderText,
+  getAllByPlaceholderText
+} from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from './store/index';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(
+test('renders name input', () => {
+  const { getByPlaceholderText } = render(
     <Provider store={store}>
       <App />
     </Provider>
   );
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const nameInput = getByPlaceholderText('name');
+  expect(nameInput).toBeInTheDocument();
 });
