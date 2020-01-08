e6f361bb97c95534b5451a5108276587942f26e0
ericauv <eriauv17@gmail.com>
2020-01-08 10:42:55 -0500

    add @types/styled-components, add to ObserverPattern

--- src/components/ObserverPattern.tsx
+++ src/components/ObserverPattern.tsx
@@ -1,5 +1,6 @@
 import React, { ChangeEvent, FormEvent } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import styled from 'styled-components';
 import {
   IInitialState,
   IRecord,
@@ -37,41 +38,7 @@ const ObserverPattern: React.FC<IProps> = () => {
     dispatch(updateAddress(''));
   };
 
-  return (
-    <div style={{ backgroundColor: name }}>
-      <form
-        onSubmit={(e: FormEvent) => {
-          e.preventDefault();
-          handleSubmit(e);
-        }}
-      >
-        <input
-          type="text"
-          placeholder="name"
-          value={name}
-          onChange={(e: ChangeEvent<HTMLInputElement>) => {
-            dispatch(updateName(e.target.value));
-          }}
-        />
-        <input
-          type="text"
-          placeholder="address"
-          value={address}
-          onChange={(e: ChangeEvent<HTMLInputElement>) => {
-            dispatch(updateAddress(e.target.value));
-          }}
-        />
-        <button type="submit"> Submit </button>
-      </form>
-      <ul>
-        {records.map((record: IRecord, index: number) => (
-          <li key={index}>
-            {record.name} | {record.address}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default ObserverPattern;
